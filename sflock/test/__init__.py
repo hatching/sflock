cc4d042efc5cea1d0036835a8208e3327e9ac91a
Jurriaan Bremer <jbr@cuckoo.sh>
2016-07-13 01:30:20 +0200

    move tests to their own directory

--- sflock/test/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2015 Jurriaan Bremer.
-# This file is part of SFlock - http://www.sflock.org/.
-# See the file 'docs/LICENSE.txt' for copying permission.
-
-from sflock.abstracts import UnitTest
-from sflock.misc import import_plugins
-
-plugins = import_plugins(__file__, "sflock.test", globals(), UnitTest)
--- tests/test_rar.py
+++ tests/test_rar.py
@@ -1,3 +1,10 @@
 # Copyright (C) 2015 Jurriaan Bremer.
 # This file is part of SFlock - http://www.sflock.org/.
 # See the file 'docs/LICENSE.txt' for copying permission.
+
+from sflock.abstracts import UnitTest
+from sflock.config import test_file
+from sflock.unpack import Rarfile
+
+class RarfileTestCase(UnitTest):
+    pass
