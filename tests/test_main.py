b23c5eeac86aeb7d1103c4d21e33b206df06f269
Jurriaan Bremer <jbr@cuckoo.sh>
2016-09-10 13:52:47 +0200

    document coverage using coveralls

---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2016 Jurriaan Bremer.
+# This file is part of SFlock - http://www.sflock.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from sflock.main import supported
+
+def test_supported():
+    assert supported()
