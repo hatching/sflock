0eacf0edf83a0682fcb270e63b8b7c8006c62970
Jurriaan Bremer <jbr@cuckoo.sh>
2017-09-20 11:10:56 +0200

    version 0.2.17

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="SFlock",
-    version="0.2.16",
+    version="0.2.17",
     author="Jurriaan Bremer",
     author_email="jbr@cuckoo.sh",
     packages=[
