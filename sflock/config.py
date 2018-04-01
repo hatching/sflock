b101dfb79c06ba34c382fe98ca8533f839b10099
Jurriaan Bremer <jbr@cuckoo.sh>
2018-04-01 15:04:12 +0200

    lazy-load pkg_resources for improved startup time

--- sflock/config.py
+++ sflock/config.py
@@ -1,10 +1,9 @@
-# Copyright (C) 2015-2016 Jurriaan Bremer.
+# Copyright (C) 2015-2018 Jurriaan Bremer.
 # This file is part of SFlock - http://www.sflock.org/.
 # See the file 'docs/LICENSE.txt' for copying permission.
 
-import pkg_resources
-
 def iter_passwords():
+    import pkg_resources
     filepath = pkg_resources.resource_filename("sflock", "data/password.txt")
     for line in open(filepath, "rb"):
         yield line.strip()
