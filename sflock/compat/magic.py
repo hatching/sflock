d52d71942ace2ceeb531450a0d2573837d3b8048
Jurriaan Bremer <jbr@cuckoo.sh>
2018-06-03 01:44:55 +0200

    ship win64 binaries for libmagic
    Thanks to Sean Gillespie [1]. Now sflock supports magic(5) on each
    platform that Cuckoo supports we can finally use it directly (as opposed
    to the duplicate cuckoo.compat.magic layer that Cuckoo features).
    
    The other reason Cuckoo didn't already use this functionality seems to
    be the fact that it was created when Cuckoo didn't support version
    pinning yet, i.e., it wasn't a Python package yet and as such
    environments with sflock already installed without updating sflock
    would've broken due to the usage of a new API endpoint.
    
    [1]: https://github.com/pidydx/libmagicwin64

--- sflock/compat/magic.py
+++ sflock/compat/magic.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 Jurriaan Bremer.
+# Copyright (C) 2016-2018 Jurriaan Bremer.
 # This file is part of SFlock - http://www.sflock.org/.
 # See the file 'docs/LICENSE.txt' for copying permission.
 
@@ -17,20 +17,18 @@ log = logging.getLogger(__name__)
 # Provide libmagic support in terms of binaries under Windows.
 if sys.platform == "win32":
     if sys.maxsize != 0x7fffffff:
-        log.warning("libmagic is not supported on 64-bit Python on Windows")
-        supported = False
+        os.environ["PATH"] = "%s;%s" % (
+            data_file("win64"), os.environ["PATH"]
+        )
+        magic_file = data_file("win64", "magic.mgc")
     else:
-        supported = True
-
         os.environ["PATH"] = "%s;%s" % (
             data_file("win32"), os.environ["PATH"]
         )
-else:
-    supported = True
+        magic_file = data_file("win32", "magic.mgc")
 
 # Therefore only import libmagic at this point.
-if supported:
-    import magic
+import magic
 
 def patch():
     """Patch libmagic to use our magic.mgc file, so that it the same across
@@ -38,25 +36,13 @@ def patch():
     if sys.platform != "win32" or magic._instances:
         return
 
-    magic._instances[False] = magic.Magic(
-        mime=False, magic_file=data_file("win32", "magic.mgc")
-    )
-
-    magic._instances[True] = magic.Magic(
-        mime=True, magic_file=data_file("win32", "magic.mgc")
-    )
+    magic._instances[False] = magic.Magic(mime=False, magic_file=magic_file)
+    magic._instances[True] = magic.Magic(mime=True, magic_file=magic_file)
 
 def from_file(*args, **kwargs):
-    if not supported:
-        return ""
-
     patch()
     return magic.from_file(*args, **kwargs)
 
 def from_buffer(*args, **kwargs):
-    if not supported:
-        return ""
-
     patch()
     return magic.from_buffer(*args, **kwargs)
-
