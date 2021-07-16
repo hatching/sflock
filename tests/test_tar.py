086aa90104f184e3875ae84710b891c37f0dffb5
Andriy Brukhovetskyy <doommedraven@gmail.com>
2021-07-16 14:42:23 +0200

    Update test_tar.py

--- tests/test_tar.py
+++ tests/test_tar.py
@@ -54,13 +54,13 @@ class TestTarFile(object):
         assert not t.f.selected
         files = list(t.unpack())
         assert len(files) == 2
-        assert files[0].relapath == b"sflock2.txt"
-        assert files[0].contents == b"sflock_plain_tar2\n"
+        assert files[0].relapath == b"sflock.txt"
+        assert files[0].contents == b"sflock_plain_tar\n"
         assert files[0].magic == "ASCII text"
         assert files[0].parentdirs == []
         assert not files[0].selected
-        assert files[1].relapath == b"sflock.txt"
-        assert files[1].contents == b"sflock_plain_tar\n"
+        assert files[1].relapath == b"sflock2.txt"
+        assert files[1].contents == b"sflock_plain_tar2\n"
         assert files[1].magic == "ASCII text"
         assert files[1].parentdirs == []
         assert not files[1].selected
