e7eb38e466d4d30579a622ad0cd31962aed6db05
Andriy Brukhovetskyy <doommedraven@gmail.com>
2021-07-16 16:51:41 +0200

    lzip

--- tests/test_lzip.py
+++ tests/test_lzip.py
@@ -3,8 +3,7 @@
 # See the file 'docs/LICENSE.txt' for copying permission.
 
 import os.path
-import pytest
-
+from sflock.main import unpack
 from sflock.abstracts import File
 from sflock.unpack import LzipFile
 
@@ -28,17 +27,8 @@ class TestLzipFile(object):
         assert files[0].parentdirs == []
         assert not files[0].selected
 
-    @pytest.mark.skip("Skipping, we don't want malware in repo: bde2d06bb333f419fff16376fffce1ee434fa47ca57c4fcb80b2932f86892e40")
-    def test_lzip_malware(self):
-        assert "lzip compressed data" in f(b"document3230.vbe.lz").magic
-        t = LzipFile(f(b"document3230.vbe.lz"))
-        assert t.handles() is True
-        assert not t.f.selected
-        files = list(t.unpack())
-        assert len(files) == 1
-        assert not files[0].filepath
-        assert files[0].relapath == b"document3230.vbe"
-        assert len(files[0].contents) == 748345
-        assert "UTF-8 Unicode text, with CRLF line terminators" == files[0].magic
-        assert files[0].parentdirs == []
-        assert not files[0].selected
+    def test_embed_lzip():
+        t = unpack(b"tests/files/test.vbe.lz.zip")
+        assert t.filename == b"test.vbe.lz.zip"
+        assert t.children[0].filename == b"document3230.vbe.lz"
+        assert t.children[0].children[0].sha256 == "36ef14835a9d2c8fe241286a7758b7f849bdabccc698e7e78318abfb195dc1db"
