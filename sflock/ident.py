ae1216cb1aec83d345a9f9acbd3c746c38cbd3c7
Andriy Brukhovetskyy <doommedraven@gmail.com>
2021-07-22 22:22:25 +0200

    Update ident.py

--- sflock/ident.py
+++ sflock/ident.py
@@ -13,7 +13,7 @@ file_extensions = OrderedDict(
         ("msi", (b".msi", b".msp", b".appx")),
         ("pub", (b".pub",)),
         ("doc", (b".doc", b".dot", b".docx", b".dotx", b".docm", b".dotm", b".docb", b".rtf", b".mht", b".mso", b".wbk")),
-        ("xls", (b".xls", b".xlt", b".xlm", b".xlsx", b".xltx", b".xlsm", b".xltm", b".xlsb", b".xla", b".xlam", b".xll", b".xlw", b".slk")),
+        ("xls", (b".xls", b".xlt", b".xlm", b".xlsx", b".xltx", b".xlsm", b".xltm", b".xlsb", b".xla", b".xlam", b".xll", b".xlw", b".slk", b".xll", b".csv")),
         ("ppt", (b".ppt", b".ppa", b".pot", b".pps", b".pptx", b".pptm", b".potx", b".potm", b".ppam", b".ppsx", b".ppsm", b".sldx", b".sldm")),
         ("jar", (b".jar",)),
         # ("rar", (b".rar",)),
@@ -38,7 +38,6 @@ file_extensions = OrderedDict(
         ("msbuild", (b".csproj", b".vbproj", b".vcxproj", b".dbproj", b".fsproj")),
         ("zip", (b".zip",)),
         ("cpl", (b".cpl",)),
-        ("xls", (b".xll",)),
         ("ichitaro", (b".jtd", b".jtdc", b".jttc", b".jtt"))
     ]
 )
