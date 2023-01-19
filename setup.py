2a5db77d55d46145f82a0a07b4616006d570b579
Andriy Brukhovetskyy <doommedraven@gmail.com>
2023-01-19 08:54:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "olefile>=0.43",
         # "peepdf>=0.4.1",
         "python-magic>=0.4.13",
-        # "yara-python>=4.1.0",
+        "yara-python>=4.1.0",
         "unicorn>=2.0.0",
         "pefile",
     ],
